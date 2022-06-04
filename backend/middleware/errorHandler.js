e3c1a52c513bd7cd3756d71990d96101878a3ee5
Ahmad Nabil <ahmednmourad@gmail.com>
2022-06-04 02:55:14 +0200

    Fixed a typo in error handler and updated apis endpoint

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,4 +1,4 @@
-export default (err, req, res) => {
+export default (err, req, res, next) => {
   const statusCode = err.statusCode || 500
   console.error(err.message, err.stack)
   res.status(statusCode).json({ message: err.message, error: err.stack })
