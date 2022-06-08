8676fb0177a30fc151379c0913ea78ac74e026ad
Mohamed Elshamy <mohamedelshamypts30@gmail.com>
2022-06-08 16:26:06 +0200

    adding validation to upload api

--- backend/controllers/upload.js
+++ backend/controllers/upload.js
@@ -1,3 +1,4 @@
+import { CustomError } from "../util/error.js"
 export default (req, res) => {
   console.log(req.files)
   const data = req.files.map((file) => {
@@ -6,5 +7,6 @@ export default (req, res) => {
       url: file.location
     }
   })
+  if (!data[0]) throw new CustomError(400, "No photo Provided")
   return res.status(200).json({ message: "uploaded successfully", data })
 }
