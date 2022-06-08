dfadd66b5136634681d9d28c658e09122e669d20
Mohamed Elshamy <mohamedelshamypts30@gmail.com>
2022-06-08 22:54:42 +0200

    chane picturre api fixed

--- backend/controllers/Profile/changeProfilePicture.js
+++ backend/controllers/Profile/changeProfilePicture.js
@@ -2,7 +2,7 @@ import connection from "../../util/mysql.js"
 
 export default async (req, res, next) => {
   const id = req.userId
-  const picture = req.body
+  const picture = req.body.picture
   try {
     await connection.query("UPDATE user SET picture = ? WHERE userId = ?", [picture, id])
     return res.status(200).json({ message: "Profile picture changed successfully", picture })
