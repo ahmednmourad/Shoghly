4ab93d13e982e687109ca9bb9815c4137efa83af
Mohamed Elshamy <mohamedelshamypts30@gmail.com>
2022-06-08 22:43:32 +0200

    delete profile picture added

--- backend/controllers/Profile/changeProfilePicture.js
+++ backend/controllers/Profile/changeProfilePicture.js
@@ -1,14 +1,11 @@
 import connection from "../../util/mysql.js"
-import { CustomError } from "../../util/error.js"
 
 export default async (req, res, next) => {
   const id = req.userId
   const picture = req.body
   try {
-    if (!picture) throw new CustomError(400, "picture not provided")
-
     await connection.query("UPDATE user SET picture = ? WHERE userId = ?", [picture, id])
-    return res.status(200).json({ message: "Profile picture changed successfully" })
+    return res.status(200).json({ message: "Profile picture changed successfully", picture })
   } catch (err) {
     next(err)
   }
