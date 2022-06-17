b72d2d4c979d751606cd4d7b9f8e1ac8ceee1632
Mohamed Elshamy <mohamedelshamypts30@gmail.com>
2022-06-17 16:24:13 +0200

    getProject updated!

--- backend/controllers/Project/getProject.js
+++ backend/controllers/Project/getProject.js
@@ -5,11 +5,10 @@ export default async (req, res, next) => {
   const projectId = req.params.projectId
 
   try {
-    const [rows] = await connection.query("SELECT project.projectId ,project.workerId ,project.description ,project.createdAt, project.updatedAt, picture.url as pictures from project LEFT JOIN picture ON project.projectId = picture.projectId WHERE project.projectId = ?", [projectId])
+    const [rows] = await connection.query("SELECT project.projectId ,project.workerId ,project.description ,project.createdAt, project.updatedAt, json_arrayagg(picture.url) as photos FROM project JOIN picture using(projectId) WHERE projectId = ? group by project.projectId", [projectId])
     const project = rows[0]
     if (!project) throw new CustomError(404, "project not found")
-    const [photos] = await connection.query("SELECT url from picture WHERE projectId = ? ", [projectId])
-    return res.status(200).json({ project, photos })
+    return res.status(200).json({ project })
   } catch (err) {
     next(err)
   }
