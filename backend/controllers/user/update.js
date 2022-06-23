02e801c50557ae020773cfce9a4f98f92bcf7faf
Ahmad Nabil <ahmednmourad@gmail.com>
2022-06-23 06:49:02 +0200

    Fixed picture update in user API

--- backend/controllers/user/update.js
+++ backend/controllers/user/update.js
@@ -2,10 +2,10 @@ import User from "../../services/user.js"
 
 export default async (req, res, next) => {
   const userId = req.userId
-  const { firstName, lastName, phone, gender, country, city, line } = req.body
+  const { firstName, lastName, phone, gender, country, city, line, picture } = req.body
 
   try {
-    const user = { firstName, lastName, phone, gender, country, city, line }
+    const user = { firstName, lastName, phone, gender, country, city, line, picture }
     await User.update(userId, user)
     console.log(`User ${userId} updated successfully`)
 
