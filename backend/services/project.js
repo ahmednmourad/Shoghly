6be2aa55ce3b7e49ec619d9519009cfa6f006d5c
Ahmad Nabil <ahmednmourad@gmail.com>
2022-07-11 23:28:39 +0200

    Added createdAt, updatedAt fields

--- backend/services/project.js
+++ backend/services/project.js
@@ -14,7 +14,7 @@ const addPhotos = async (photos) => {
 }
 
 const list = async (workerId) => {
-  return await sequelize.query("SELECT projectId id, description, json_array((SELECT url FROM picture where picture.projectId = project.projectId limit 1)) as pictures FROM project WHERE workerId = ?", {
+  return await sequelize.query("SELECT projectId id, description, json_array((SELECT url FROM picture where picture.projectId = project.projectId limit 1)) as pictures, createdAt, updatedAt FROM project WHERE workerId = ?", {
     replacements: [workerId],
     type: sequelize.QueryTypes.SELECT,
     raw: true
