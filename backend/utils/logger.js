c388aca20275973f42b9c966d24d0f0e2a417591
Ahmad Nabil <ahmednmourad@gmail.com>
2022-07-12 01:22:35 +0200

    update: timestamp, hooks in logger

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,13 +1,23 @@
 import pino from "pino"
 
 const logger = pino({
-  timestamp: false,
+  timestamp: pino.stdTimeFunctions.isoTime,
   base: undefined,
   formatters: { level: (label) => { return { level: label.toUpperCase() } } },
   transport: {
     target: "pino-pretty",
     options: {
       colorize: true
+    }
+  },
+  hooks: {
+    logMethod (inputArgs, method, level) {
+      if (inputArgs.length >= 2) {
+        const arg1 = inputArgs.shift()
+        const arg2 = inputArgs.shift()
+        return method.apply(this, [arg2, arg1, ...inputArgs])
+      }
+      return method.apply(this, inputArgs)
     }
   }
 })
