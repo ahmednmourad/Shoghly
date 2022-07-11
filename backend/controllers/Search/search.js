c6b3919a7c6e3df554a4c6a720b8751ba6afbb4e
Ahmad Nabil <ahmednmourad@gmail.com>
2022-07-11 23:28:07 +0200

    Added profession filter

--- backend/controllers/Search/search.js
+++ backend/controllers/Search/search.js
@@ -2,13 +2,16 @@ import connection from "../../utils/mysql.js"
 import { CustomError } from "../../utils/error.js"
 
 export default async (req, res, next) => {
-  const text = req.query.text
-  const city = req.query.city
+  const { text, city, profession } = req.query
 
   try {
     if (!text) throw new CustomError(400, "Empty text field")
     if (!city) throw new CustomError(400, "No city Provided")
-    const [rows] = await connection.query("SELECT userId, CONCAT(firstName, ' ', lastName) as 'fullName', picture, profession, phone, city, line, email,  (SELECT AVG(rating) FROM review WHERE review.workerId = user.userId) as rating FROM user LEFT JOIN review ON user.userId = review.workerId WHERE CONCAT(firstName, ' ', lastName) LIKE ? AND role = 'worker' AND city = ?", ["%" + text + "%", city])
+    const [rows] = await connection.query(`SELECT userId, CONCAT(firstName, ' ', lastName) as 'fullName', picture, profession, phone, city, line, email,  (SELECT AVG(rating) 
+    FROM review WHERE review.workerId = user.userId) as rating 
+    FROM user 
+    LEFT JOIN review ON user.userId = review.workerId 
+    WHERE CONCAT(firstName, ' ', lastName) LIKE ? AND role = 'worker' AND city = ? ${profession ? `AND profession = ${connection.escape(profession)}` : ""}`, ["%" + text + "%", city])
     const results = rows
     res.status(200).json({ message: "success", results })
   } catch (err) {
